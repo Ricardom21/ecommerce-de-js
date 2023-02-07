ec10691c0a868ecbd74d744a3dcda139b38a1bb1
ricardo.m21 <ricardoabraham00@gmail.com>
2023-02-07 01:51:28 -0300

    agregando el carrito

--- apps.js
+++ apps.js
@@ -1,7 +1,70 @@
-// Creando arrays de productos
+let carrito = []
+const contenedor = document.getElementById('productos')
+let valorTotal = 0
+// pintar el arrays
 
-const productos = [
-    {
+const pintarProductos = () => {
+    let index = 0
+    productos.forEach((productos) => {
+        const div = document.createElement('div')
+        div.classList.add('productos')
+        div.innerHTML = `
+        <div class="container">
+        <div class="card-flex">
+            <div class="card-info">
+                <div class="product-image">
+                    <img src="${productos.img}" alt="${productos.titulo}">
+                </div>
+                <h3>${productos.nombre}</h3>
+                <span>⭐⭐⭐⭐⭐</span>
+                <p>${productos.precio} $</p>
+                <button onclick="aggCarrito(${index})" class="producto-agregar" id="${productos.id}">Agregar</button>
+            </div>
+        `
+        contenedor.appendChild(div)
+            index++
         
-    }
-]
+     })
+};
+
+pintarProductos()
+
+// funciones
+function aggCarrito (producto) {
+    valorTotal += productos[producto].precio
+    let valorCarrito = document.getElementById("valorCarrito");
+    carrito.push(productos[producto])
+    valorCarrito.textContent=carrito.length;
+    localStorage.setItem('carrito', carrito);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ productos.js
@@ -0,0 +1,68 @@
+// arrays de productos
+
+const productos = [
+    {
+        id:1,
+        nombre: "Coctelera",
+        categoria: "bartender",
+        precio: 2500,
+        img: "img/coctelera.webp"
+
+    },
+    {
+        id:2,
+        nombre: "Jigger",
+        categoria: "bartender",
+        precio: 1500,
+        img: "img/jigger.jpg"
+
+    },
+    {
+        id:3,
+        nombre: "Cuchara",
+        categoria: "bartender",
+        precio: 3500,
+        img: "img/cuchara.jpg"
+
+    },
+    {
+        id:4,
+        nombre: "Dosificador",
+        categoria: "bartender",
+        precio: 500,
+        img: "img/dosificador.jpg"
+
+    },
+    {
+        id:5,
+        nombre: "Mortero",
+        categoria: "bartender",
+        precio: 5500,
+        img: "img/mortero.webp"
+
+    },
+    {
+        id:6,
+        nombre: "Copas",
+        categoria: "bartender",
+        precio: 10000,
+        img: "img/copas.jpg"
+
+    },
+    {
+        id:7,
+        nombre: "Esterilla",
+        categoria: "bartender",
+        precio: 6500,
+        img: "img/gomabarra.jpg"
+
+    },
+    {
+        id:8,
+        nombre: "Escarchador",
+        categoria: "bartender",
+        precio: 7500,
+        img: "img/escarchador.webp"
+
+    },
+]
